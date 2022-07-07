86637216ed47dad89fa533f00f08bb646ddde4ee
BSDashLoko <70023741+BSDashLoko@users.noreply.github.com>
2022-07-07 18:31:58 -0300

    All files added to theproject

---
+++ resetCom.js
@@ -0,0 +1,34 @@
+const fs = require('node:fs');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId, token } = require('./config.json');
+
+
+const rest = new REST({ version: '9' }).setToken(token);
+rest.get(Routes.applicationGuildCommands(clientId, guildId))
+    .then(data => {
+        const promises = [];
+        for (const command of data) {
+            const deleteUrl = `${Routes.applicationGuildCommands(clientId, guildId)}/${command.id}`;
+            promises.push(rest.delete(deleteUrl));
+        }
+        return Promise.all(promises);
+    });
+
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+}
+
+rest.put(Routes.applicationCommands(clientId, guildId),{ body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
+
+
+
+    
+//node resetCom.js
